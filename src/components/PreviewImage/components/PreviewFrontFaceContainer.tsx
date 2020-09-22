872c78aebdf971e87811b68ddd5256908bbf6c37
grettaface <bret.kruse@gmail.com>
2020-09-21 23:02:12 -0400

    Adjust object fit

--- src/components/PreviewImage/components/PreviewFrontFaceContainer.tsx
+++ src/components/PreviewImage/components/PreviewFrontFaceContainer.tsx
@@ -39,6 +39,9 @@ const PreviewFrontFaceContainer = styled.div`
 
   img {
     object-fit: cover;
+    width: 100%;
+    height: 100%;
+    object-position: center center;
   }
 `
 export default PreviewFrontFaceContainer
